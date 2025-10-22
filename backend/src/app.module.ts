5fd2c5a950bf2de5125e250c92f068eb649dfe30
benisamuel <benisamuel566@gmail.com>
2025-10-22 18:49:33 +0200

    include item module in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './models/item.model';
+import { ItemModule } from './modules/items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'beni@ish1',
+      database: 'fashiq',
+      entities: [Item],
+      synchronize: true,
+    }),
+    ItemModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
