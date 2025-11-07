389afa0d610c2547edd414cc9bb14c90ffb5ab8d
benisamuel <benisamuel566@gmail.com>
2025-11-07 22:14:06 +0200

    improved cart columns

--- backend/src/models/cart.model.ts
+++ backend/src/models/cart.model.ts
@@ -11,9 +11,6 @@ export class Cart {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: Date, default: Date.now() })
-  created_at: Date;
-
   @OneToMany(() => Item, (item) => item.cart)
   items: Item[];
 }
